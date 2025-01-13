199cb3fde673254e7c3fe1f922d249f482294403
EugineI <eugineindimuli@gmail.com>
2025-01-13 23:02:08 +0300

    1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,6 @@
 #installing flask using puppet
-package { 'Flask':
-ensure   => '2.1.0',
-provider => 'pip3',
+exec { 'install_flask':
+command => '/usr/bin/pip3 install flask==2.1.0',
+path    => '/usr/bin:/bin:/usr/sbin:/sbin',
+unless  => '/usr/bin/pip3 show flask | grep -q "version: 2.1.0"',
 }
