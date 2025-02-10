c25747c1744e6a78e4e271d2c776f36b296f889e
EugineI <eugineindimuli@gmail.com>
2025-02-10 12:54:16 +0300

    fetching data

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+fetches done tasks from todo-lists
+"""
+import requests
+import sys
+employee_id = int(sys.argv[1])
+
+base_url = "https://jsonplaceholder.typicode.com"
+user_url = f"{base_url}/users/{employee_id}"
+user_response = requests.get(user_url)
+
+user_data = user_response.json()
+employee_name = user_data.get("name")
+
+todos_url = f"{base_url}/todos?userId={employee_id}"
+todos_response = requests.get(todos_url)
+
+todos = todos_response.json()
+ttTasks = len(todos)
+done_tasks = [task for task in todos if task.get("completed")]
+done_task = len(done_tasks)
+
+print(f"Employee {employee_name} is done with tasks ({done_task}/{ttTasks}):")
+for task in done_tasks:
+    print(f"\t {task['title']}")
