c628ced7a1b8b313cb582740953515da6dbec6c9
EugineI <eugineindimuli@gmail.com>
2025-02-26 19:41:55 +0300

    init.pp

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,2 +1,27 @@
 # fix 500 error
-include apache_fix
+package { 'apache2':
+  ensure => installed,
+}
+
+package { 'libapache2-mod-php':
+  ensure => installed,
+}
+
+file { '/var/www/html/':
+  ensure  => directory,
+  owner   => 'www-data',
+  group   => 'www-data',
+  recurse => true,
+}
+
+exec { 'enable-mod-rewrite':
+  command => '/usr/sbin/a2enmod rewrite',
+  unless  => '/bin/grep -q "rewrite.load" /etc/apache2/mods-enabled/',
+  notify  => Service['apache2'],
+}
+
+service { 'apache2':
+  ensure  => running,
+  enable  => true,
+  require => [Package['apache2'], Exec['enable-mod-rewrite']],
+}
